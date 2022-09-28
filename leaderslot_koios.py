0a56de858f9333571f5889699a9877435d636b4a
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-09-28 13:06:43 +0200

    main

--- leaderslot_koios.py
+++ leaderslot_koios.py
@@ -187,7 +187,7 @@ firstSlot = firstShelleySlot[0]["abs_slot"]
 
 
 ### calculate first slot of target epoch ###
-firstSlotOfEpoch = float(firstSlot) + float(epoch - 211) * float(epochLength)
+firstSlotOfEpoch = (firstSlot) + (epoch - 211) * (epochLength)
 
 from decimal import *
 getcontext().prec = 9
